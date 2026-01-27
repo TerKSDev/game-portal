ff3dcc627aceb3de9a038d35fa30acf69c6af4ee
HuoYan1213 <huoyan0928@gmail.com>
2026-01-27 19:30:59 +0800

    Fix: build failed problem

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,27 +1,42 @@
 import Header from "@/app/components/Header";
 import LoginForm from "./LoginForm";
+import { Suspense } from "react";
 
 import { ROUTES } from "@/app/_config/routes";
 
 function GetTitle(path: string) {
-  const route = ROUTES.find(r => r.path === path);
+  const route = ROUTES.find((r) => r.path === path);
   return route ? route.name : "Game Portal";
 }
 
 export const metadata = {
-  title: GetTitle('/login'),
-}
+  title: GetTitle("/login"),
+};
 
 export default function Login() {
-    return (
-        <div className="flex flex-1 w-screen h-screen" style={ { backgroundImage: 'url(/background/login-background.jpg)', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center' } }>
-            <div className="w-screen h-screen bg-black opacity-60 fixed top-0 left-0 z-0"></div>
+  return (
+    <div
+      className="flex flex-1 w-screen h-screen"
+      style={{
+        backgroundImage: "url(/background/login-background.jpg)",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="w-screen h-screen bg-black opacity-60 fixed top-0 left-0 z-0"></div>
 
-            <div className="flex flex-col m-auto font-mono w-3/4 max-w-150 opacity-95 z-10">
-                <h1 className="text-2xl mb-4 ml-1">Welcome Back !</h1>
+      <div className="flex flex-col m-auto font-mono w-3/4 max-w-150 opacity-95 z-10">
+        <h1 className="text-2xl mb-4 ml-1">Welcome Back !</h1>
 
-                <LoginForm />
-            </div>
-        </div>
-    );
+        <Suspense
+          fallback={
+            <div className="bg-gray-900 flex flex-col rounded p-8 gap-y-4 min-h-96" />
+          }
+        >
+          <LoginForm />
+        </Suspense>
+      </div>
+    </div>
+  );
 }
