4ff177eb47e03940410cb17d82820a9bff3770b6
HuoYan1213 <huoyan0928@gmail.com>
2026-01-30 16:34:35 +0800

    Update: openGraph image resize

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,17 +22,8 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Game Portal",
     description: "The best place to find your next adventure...",
-    url: "https://game-portal-gamma.vercel.app",
     siteName: "Game Portal",
-    images: [
-      {
-        url: "https://game-portal-gamma.vercel.app/og-image.png",
-        width: 1200,
-        height: 630,
-        alt: "Game Portal Open Graph Image",
-        type: "image/png",
-      },
-    ],
+    images: ["/og-image.png"],
     type: "website",
   },
 };
